7b4899e4c95bf1492dce8c6df855cf5ea8bae104
hyeonee <yhe01152@naver.com>
2024-02-01 12:30:15 +0900

    [fix] post_detail script 파일 분할

--- server/static/posts/js/comment.js
+++ server/static/posts/js/comment.js
@@ -56,194 +56,117 @@ function writeComment(post_id) {
 requestCommentAdd.onreadystatechange = () => {
   if (requestCommentAdd.readyState === XMLHttpRequest.DONE) {
     if (requestCommentAdd.status < 400) {
-      // const { commenter, content, commentId, post_id } = JSON.parse(
-      //   requestCommentAdd.response
-      // );
-      const { content, commentId, post_id } = JSON.parse(
+      const { commenter, content, commentId, post_id } = JSON.parse(
         requestCommentAdd.response
       );
       const element = document.querySelector(".commentSection");
       let originHTML = element.innerHTML;
       element.innerHTML += `<div class="a_comment Cid-${commentId}">
-          <div class="a_comment_commenter">commenter</div>
-          <div class="a_comment_content">${content}</div>
-          <button
-            class="a_comment_delete"
-            onclick="deleteComment(${commentId})"
-          >
-            삭제
-          </button>
-          <button
-            class="a_comment_delete"
-            onclick="updateComment(${commentId})"
-          >
-            수정
-          </button>
-          <button
-            class="a_comment_delete"
-            onclick="updateComment(${commentId})"
-          >
-            좋아요
-          </button>
-          <button
-            class="a_comment_reply"
-            onclick="replyComment(${post.id}, ${commentId})"
-          >
-            답글
-          </button>
-          </div>`;
+            <div class="a_comment_commenter">${commenter}</div>
+            <div class="a_comment_content">${content}</div>
+            <button
+              class="a_comment_delete"
+              onclick="deleteComment(${commentId})"
+            >
+              삭제
+            </button>
+            <button
+              class="a_comment_delete"
+              onclick="updateCommentBtn(${commentId})"
+            >
+              수정
+            </button>
+            <button
+              class="a_comment_delete"
+              onclick="likeComment(${commentId})"
+            >
+              좋아요
+            </button>
+            <button
+              class="a_comment_reply"
+              onclick="writeReply(${post_id}, ${commentId})"
+            >
+              답글
+            </button>
+            </div><section class="replySection"></section>`;
     }
   }
 };
 
-// 대댓글 열람 부분
-
+// 댓글 업데이트 부분
 //새 HTTPRequest 생성
-const requestReplyAdd = new XMLHttpRequest();
+const requestCommentUpdate = new XMLHttpRequest();
 
-// 함수명 : writeReply
-// 전달인자 : post_id
+// 함수명 : updateComment
+// 전달인자 : comment_id
 // 기능 : 서버에 댓글 작성 요청 및 댓글 내용 전달
-function openReply(post_id, parent_comment_id) {
-  const url = `/communitys/comment_create/`;
-  requestReplyAdd.open("POST", url, true);
-  requestReplyAdd.setRequestHeader(
+function updateComment(comment_id) {
+  const content = document.querySelector(".comment-update-box");
+
+  const url = `/communitys/comment_update/${comment_id}/`;
+  requestCommentUpdate.open("POST", url, true);
+  requestCommentUpdate.setRequestHeader(
     "Content-Type",
     "application/x-www-form-urlencoded"
   );
-  requestReplyAdd.send(
+  requestCommentUpdate.send(
     JSON.stringify({
-      post_id: post_id,
-      parent_comment_id: parent_comment_id,
-    })
-  );
-}
-
-// 대댓글 열람 요청 응답 온 후
-requestReplyAdd.onreadystatechange = () => {
-  if (requestReplyAdd.readyState === XMLHttpRequest.DONE) {
-    if (requestReplyAdd.status < 400) {
-      // const { commenter, content, commentId, post_id } = JSON.parse(
-      //   requestReplyAdd.response
-      // );
-      const { content, commentId, post_id, flag } = JSON.parse(
-        requestReplyAdd.response
-      );
-      const element = document.querySelector(".commentSection");
-      let originHTML = element.innerHTML;
-      element.innerHTML += `<div class="a_comment Cid-${commentId}">
-          <div class="a_comment_commenter">commenter</div>
-          <div class="a_comment_content">${content}</div>
-          <button
-            class="a_comment_delete"
-            onclick="deleteComment(${commentId})"
-          >
-            삭제
-          </button>
-          <button
-            class="a_comment_delete"
-            onclick="updateComment(${commentId})"
-          >
-            수정
-          </button>
-          <button
-            class="a_comment_delete"
-            onclick="updateComment(${commentId})"
-          >
-            좋아요
-          </button>
-          <button
-            class="a_comment_reply"
-            onclick="replyComment(${post.id}, ${commentId})"
-          >`;
-    }
-  }
-};
-
-// 대댓글 탭 닫기
-function closeReply(post_id, parent_comment_id) {
-  const content = document.querySelector(".comment-input-box");
-
-  const url = `/communitys/comment_create/`;
-  requestReplyAdd.open("POST", url, true);
-  requestReplyAdd.setRequestHeader(
-    "Content-Type",
-    "application/x-www-form-urlencoded"
-  );
-  requestReplyAdd.send(
-    JSON.stringify({
-      post_id: post_id,
+      comment_id: comment_id,
       content: content.value,
-      parent_comment_id: parent_comment_id,
     })
   );
   content.value = "";
 }
 
-// 대댓글 추가 부분
-
-//새 HTTPRequest 생성
-const requestReplyAdd = new XMLHttpRequest();
-
-// 함수명 : writeReply
-// 전달인자 : post_id
-// 기능 : 서버에 댓글 작성 요청 및 댓글 내용 전달
-function writeReply(post_id, parent_comment_id) {
-  const content = document.querySelector(".comment-input-box");
-
-  const url = `/communitys/comment_create/`;
-  requestReplyAdd.open("POST", url, true);
-  requestReplyAdd.setRequestHeader(
-    "Content-Type",
-    "application/x-www-form-urlencoded"
-  );
-  requestReplyAdd.send(
-    JSON.stringify({
-      post_id: post_id,
-      content: content.value,
-      parent_comment_id: parent_comment_id,
-    })
-  );
-  content.value = "";
-}
-
-// 대댓글 추가 요청 응답 온 후
-requestReplyAdd.onreadystatechange = () => {
-  if (requestReplyAdd.readyState === XMLHttpRequest.DONE) {
-    if (requestReplyAdd.status < 400) {
-      // const { commenter, content, commentId, post_id } = JSON.parse(
-      //   requestReplyAdd.response
-      // );
-      const { content, commentId, post_id, flag } = JSON.parse(
-        requestReplyAdd.response
+// 댓글 업데이트 요청 응답 온 후
+requestCommentUpdate.onreadystatechange = () => {
+  if (requestCommentUpdate.readyState === XMLHttpRequest.DONE) {
+    if (requestCommentUpdate.status < 400) {
+      const { commenter, content, commentId, post_id } = JSON.parse(
+        requestCommentUpdate.response
       );
-      const element = document.querySelector(".commentSection");
-      let originHTML = element.innerHTML;
-      element.innerHTML += `<div class="a_comment Cid-${commentId}">
-          <div class="a_comment_commenter">commenter</div>
-          <div class="a_comment_content">${content}</div>
-          <button
-            class="a_comment_delete"
-            onclick="deleteComment(${commentId})"
-          >
-            삭제
-          </button>
-          <button
-            class="a_comment_delete"
-            onclick="updateComment(${commentId})"
-          >
-            수정
-          </button>
-          <button
-            class="a_comment_delete"
-            onclick="updateComment(${commentId})"
-          >
-            좋아요
-          </button>
-          <button
-            class="a_comment_reply"
-            onclick="replyComment(${post.id}, ${commentId})"
-          >`;
+      const element = document.querySelector(`.Cid-${commentId}`);
+      element.innerHTML = `<div class="a_comment_commenter">${commenter}</div>
+            <div class="a_comment_content">${content}</div>
+            <button
+              class="a_comment_delete"
+              onclick="deleteComment(${commentId})"
+            >
+              삭제
+            </button>
+            <button
+              class="a_comment_delete"
+              onclick="updateCommentBtn(${commentId})"
+            >
+              수정
+            </button>
+            <button
+              class="a_comment_delete"
+              onclick="likeComment(${commentId})"
+            >
+              좋아요
+            </button>
+            <button
+              class="a_comment_reply"
+              onclick="writeReply(${post_id}, ${commentId})"
+            >
+              답글
+            </button><section class="replySection"></section>`;
     }
   }
 };
+
+// 함수명 : updateCommentBtn
+// 전달인자 : comment_id
+// 기능 : 댓글 업데이트 버튼 입력시 input 칸으로 변경
+function updateCommentBtn(comment_id) {
+  const element = document.querySelector(`.Cid-${comment_id}`);
+  element.innerHTML = `<div class="input-area">
+        <input type="text" class="comment-update-box" /><button
+          class="comment-upload-btn"
+          onclick="updateComment(${comment_id})"
+        >
+          수정
+        </button>
+      </div>`;
+}
