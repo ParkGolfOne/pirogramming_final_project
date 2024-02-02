b21470b9049d1942956562b12e81a0029f901b6b
Phoebe125 <ted9988sm9988@gmail.com>
2024-02-02 17:54:10 +0900

    [feat] region 추가

--- server/apps/users/models.py
+++ server/apps/users/models.py
@@ -1,20 +1,24 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+# 유저 모델
 class User(AbstractUser):
     username = models.CharField('아이디', max_length=15, null=False, unique=True)
     nickname = models.CharField('닉네임', max_length=31, null=False)
     birth = models.DateField(null=True)
-    phone= models.CharField(max_length=15, null=False)
-    address= models.CharField(max_length=255, null=False)
+    phone = models.CharField(max_length=15, null=False)
+    address = models.CharField(max_length=255, null=False)
     first_login = models.BooleanField(default=True)
-    #town_id
+    region = models.ForeignKey("Region", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.username
-    
 
-# class SocialUser(models.Model):
-#     user = models.ForeignKey(
-#         allauth.app_settings.USER_MODEL, on_delete=models.CASCADE)
+# 시도/ 군구 모델
+class Region(models.Model):
+    city = models.CharField("시/도", max_length=31)
+    town = models.CharField("군/구", max_length=31)
+
+    def __str__(self):
+        return self.city + ' ' + self.town
+
--- server/apps/users/urls.py
+++ server/apps/users/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("update/<int:pk>/", views.update, name="update"),
     path("social_login/", views.social_login, name="social_login"),
-    # path("naver_logout/", views.naver_logout, name="naver_logout"),
-    # path("check_naver_auth/", views.check_naver_auth, name="check_naver_auth"),
+    path('get_town_list/', views.get_town_list, name='get_town_list'),
 ]
