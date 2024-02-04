f2236c10e3074ccd44c0d1f199858699c8d69e3f
Leeseungchan <wkdvnddl050701@gmail.com>
2024-02-05 05:49:43 +0900

    [feat] 게임 완성git add .git add .git add .git add .

--- server/apps/games/models.py
+++ server/apps/games/models.py
@@ -4,21 +4,23 @@ from apps.locations.models import *
 from django.utils import timezone
 from apps.locations.models import *
 from apps.score.models import Score
+def default_scores():
+    return {"hole1": 0, "hole2": 0, "hole3": 0, "hole4": 0, "hole5": 0, "hole6": 0, "hole7": 0, "hole8": 0, "hole9": 0}
 
 # 게임 모델 게임을 
 class Game(models.Model):
     create_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,  verbose_name='create_user')
     game_name = models.CharField('게임이름', max_length = 9, default = '게임')
     ground = models.ForeignKey(GolfLocation, null = True, blank = True ,on_delete=models.SET_NULL, verbose_name='골프장')
-    par = models.CharField('파', max_length = 9, default = '555555555')
     created_at = models.DateTimeField(default=timezone.now)
     # ROUND카운터를 따로 두지 않아도 될 듯? 역 참조로 다 불러오면 될듯
 
 class Round(models.Model):
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name='rounds')
+    par = models.JSONField(default=default_scores)
     round_number = models.IntegerField()
 
 class Player(models.Model):
     round = models.ForeignKey(Round, on_delete=models.CASCADE, related_name='round_player')
     name = models.CharField(max_length=100)
-    score = models.ForeignKey(Score, on_delete=models.CASCADE, related_name='game')
+    score = models.ForeignKey(Score, on_delete=models.CASCADE, related_name='score_player')
--- server/apps/games/views.py
+++ server/apps/games/views.py
@@ -19,7 +19,6 @@ def game_set(request):
             game = Game.objects.create(
                 create_by = request.user,
                 ground=None,  # 일단은 골프장 none
-                par='555555555',  # 일단 디폴트 값
                 created_at=timezone.now()
             )
             # Round 인스턴스 생성
@@ -37,9 +36,10 @@ def game_set(request):
 @csrf_exempt  
 def game_detail(request, game_id, round_count, player_count):
     # 페이지는 게임과 라운드 관련 모든 정보를 보여 주고 수정 가능하게
+    game = get_object_or_404(Game, id=game_id)
+    rounds = Round.objects.filter(game_id=game_id)
+
     if request.method == 'GET':
-        game = get_object_or_404(Game, id=game_id)
-        rounds = Round.objects.filter(game_id=game_id)
 
         for round in rounds:
             players = Player.objects.filter(round=round)
@@ -47,27 +47,41 @@ def game_detail(request, game_id, round_count, player_count):
                 for i in range(players.count(), player_count):
                     score = Score.objects.create(player=None, ground=game.ground)
                     Player.objects.create(round=round, name=f"플레이어{i+1}", score=score)
+        
         content = {
-            'game': game, 
+            'game': game,
+            'rounds': rounds, 
             'round_count': round_count, 
             "player_count" : player_count,
         }
         return render(request, 'games/game_detail.html', content)
+    
     # 저장 
     elif request.method == 'POST':
-        score_data = request.POST.get('score')
-        game_id = request.POST.get('game_id')
         game = get_object_or_404(Game, id=game_id)
         rounds = Round.objects.filter(game_id=game_id)
 
         for round in rounds:
-            scores = Score.objects.filter(round_id=round.id)
-            for score_instance in scores:
-                score_instance.player=None,
-                score_instance.ground=game.ground,
-                score_instance.par=game.par,
-                score_instance.score=score_data,
-                score_instance.total_score=0, 
-                score_instance.save()
-        
+            par_data = round.par
+            for i in range(1, 10):
+                hole_key = f'hole{i}'
+                par_data[hole_key] = int(request.POST.get(f'par{i}_{round.id}', 0))
+            round.par = par_data
+            round.save()
+            players = Player.objects.filter(round_id=round.id)
+            for player in players:
+                scores_data = player.score.scores 
+                for i in range(1, 10):
+                    hole_key = f'hole{i}'
+                    scores_data[hole_key] = int(request.POST.get(f'score{i}_{player.id}', 0))
+                player.score.scores = scores_data
+                player.score.par = par_data
+                player.score.ground = game.ground
+                player.score.save()
+
+    # 임시 저장이므로 다시 원래 페이지
+    return redirect('games:game_detail', game_id=game.id, round_count=round_count, player_count=player_count)
+
+# 추가 구현 --> 저장페이지에서 유저를 고르면 유저의 스코어 보드에 일괄적으로 저장 즉, 스코어의 player를 유저로 바꾸는 것
+# 친구 기능이 되거나 나중에 ㄱㄱ
         
