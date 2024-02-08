cb33f7d304fe374dcb677fefe8a8a3233b6f1999
Leeseungchan <wkdvnddl050701@gmail.com>
2024-02-08 20:20:39 +0900

    [fix] 오류 수정

--- server/apps/games/views.py
+++ server/apps/games/views.py
@@ -4,6 +4,7 @@ from .models import Game, Round, Player
 from django.utils import timezone
 from django.urls import reverse
 from apps.score.models import Score
+from apps.users.models import User
 from django.views.decorators.csrf import csrf_exempt
 
 # 게임 설정및 게임 생성 페이지
@@ -18,6 +19,8 @@ def game_set(request):
             player_count = form.cleaned_data['player_count']
         
             # Game 인스턴스 생성
+            if request.user == :
+               creater 
             game = Game.objects.create(
                 create_by = request.user,
                 ground=None,  # 일단은 골프장 none
@@ -29,17 +32,17 @@ def game_set(request):
                 Round.objects.create(game=game, round_number = (i + 1) )
 
             # url에 라운드 수와 플레이어 수를 저장하고 game_detail로 보내기
-            return redirect(reverse('games:game_detail', args=(game.id,player_count)))
+            return redirect(reverse('games:game_update', args=(game.id,player_count)))
     else:
         form = GameSetupForm()
         content = {
             'form': form
         }
-        return render(request, 'games/game_set.html', content)
+        return render(request, 'games/game_create.html', content)
 
 @csrf_exempt  
 # game_detail은 게임과 라운드 관련 모든 정보를 보여 주고 수정 가능하게    
-def game_detail(request, game_id, player_count):
+def game_update(request, game_id, player_count):
     
     # game_set에서 만든 게임과 라운드들 가져오기
     game = get_object_or_404(Game, id=game_id)
@@ -49,7 +52,7 @@ def game_detail(request, game_id, player_count):
     if request.method == 'GET':
         # 불러온 라운드에 대해
         for round in rounds:
-            # 만일 플레이어 생성 중 문제시 혹은 플레이어가 이미 생성됐다면 (사실 이 부분은 지피티가 했어요....)
+            # 만일 플레이어 생성되었다면
             players = Player.objects.filter(round=round)
             if players.count() < player_count:
                 # 플레이어 수 만큼 플레이어와 플레이어의 스코어 보드도 생성
@@ -63,7 +66,7 @@ def game_detail(request, game_id, player_count):
             'rounds': rounds,  
             "player_count" : player_count,
         }
-        return render(request, 'games/game_detail.html', content)
+        return render(request, 'games/game_update.html', content)
     
     # 임시 저장 
     elif request.method == 'POST':
@@ -91,19 +94,32 @@ def game_detail(request, game_id, player_count):
                 player.score.scores = scores_data
                 player.score.par = par_data
                 player.score.ground = game.ground
+                player.save()
                 player.score.save()
-            if request.POST.get("save") == "점수 저장":
-                redirect("games:game_save")
+        if request.POST.get("save") == "점수 저장":
+            print(1)
+            return redirect("games:game_save", game_id=game.id)
     # 임시 저장이므로 다시 원래 페이지
-    return redirect('games:game_detail', game_id=game.id, player_count=player_count)
+    return redirect('games:game_update', game_id=game.id, player_count=player_count)
 
 # 추가 구현 --> 저장페이지에서 유저를 고르면 유저의 스코어 보드에 일괄적으로 저장 즉, 스코어의 player를 유저로 바꾸는 것
 # 친구 기능이 되거나 나중에 ㄱㄱ
-# 최종 저장 기능도 만들어야 해욥
 # # 이름 바꿀 수 있게 해야돼요
 def game_save(request, game_id):
     game = get_object_or_404(Game, id=game_id)
     rounds = Round.objects.filter(game_id=game_id)
-    for round in rounds:
-        players = Player.objects.filter(round_id=round.id)
-            
+    
+    if request.method == 'GET':
+        content = {
+            'game': game,
+            'rounds': rounds,
+        }
+        return render(request, 'games/game_detail.html', content)
+    
+    if request.method == 'POST':
+        for round in rounds:
+            players = Player.objects.filter(round_id=round.id)
+            for player in players:
+                get_object_or_404(User_id = request.POST.get(f'name_{player.id}'))
+                # 넘겨줄떄 id로 넘겨야 할듯
+                # player.score.player =
