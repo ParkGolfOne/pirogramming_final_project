d66e4603af15c082881962b44d2fab36b71f6d4c
hyeonee <yhe01152@naver.com>
2024-02-08 15:27:17 +0900

    [feat] 게시글 공유하기 작업 && 스코어 경로 수정,

--- server/static/js/link.js
+++ server/static/js/link.js
@@ -1,37 +1,34 @@
-
-Kakao.init('api키');
+Kakao.init("7d2565115739c70be1903fea71d797fb");
 
 //점수 공유
-function shareKakaoScore(){
-    const totalScore = document.querySelector(".totalScoreArea").value
-    const scoreAreas = document.querySelectorAll(".scoreArea")
-    let scores = []
-    scoreAreas.forEach((event)=>{
-        scores.push(event.outerText)
-    })
+function shareKakaoScore() {
+  const totalScore = document.querySelector(".totalScoreArea").value;
+  const scoreAreas = document.querySelectorAll(".scoreArea");
+  let scores = [];
+  scoreAreas.forEach((event) => {
+    scores.push(event.outerText);
+  });
 
-    Kakao.Share.createDefaultButton({
-        container: '#kakaotalk-sharing-btn',
-        objectType: 'feed',
-        content: {
-          title: '점수',
-          description: '나의 점수 공유!',
-          imageUrl:
-            thumbnailUrl,
-          link: {
-            mobileWebUrl: `http://127.0.0.1:8000/score/score_detail/${sid}/`,
-            webUrl: `http://127.0.0.1:8000/score/score_detail/${sid}/`,
-          },
+  Kakao.Share.createDefaultButton({
+    container: "#kakaotalk-sharing-btn",
+    objectType: "feed",
+    content: {
+      title: "점수",
+      description: "나의 점수 공유!",
+      imageUrl: thumbnailUrl,
+      link: {
+        mobileWebUrl: `http://127.0.0.1:8000/score/score_detail/${sid}/`,
+        webUrl: `http://127.0.0.1:8000/score/score_detail/${sid}/`,
+      },
+    },
+    buttons: [
+      {
+        title: "점수 보러가기~!!",
+        link: {
+          mobileWebUrl: `http://127.0.0.1:8000/score/score_detail/${sid}/`,
+          webUrl: `http://127.0.0.1:8000/score/score_detail/${sid}/`,
         },
-        buttons: [
-          { 
-            title: '점수 보러가기~!!',
-            link: {
-              mobileWebUrl: `http://127.0.0.1:8000/score/score_detail/${sid}/`,
-              webUrl: `http://127.0.0.1:8000/score/score_detail/${sid}/`,
-            },
-          },
-        ],
-      });
-
+      },
+    ],
+  });
 }
