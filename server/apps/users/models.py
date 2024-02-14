6cc376cab5699c6f5ed1bd51337e7872fdd0c366
Phoebe125 <ted9988sm9988@gmail.com>
2024-02-14 15:31:42 +0900

    [test] test 실패 뜨는거 해결 (유저 모델 수정)

--- server/apps/users/models.py
+++ server/apps/users/models.py
@@ -21,7 +21,7 @@ class User(AbstractUser):
     birth = models.DateField(null=True, blank=True)
     phone = models.CharField(max_length=15, null=True, blank=True)
     email = models.EmailField('이메일', max_length=255, null=False, unique=True)
-    address = models.CharField(max_length=255, null=False)
+    address = models.CharField(max_length=255, null=True)
     detail_address = models.CharField(max_length=255, null=True, blank=True)
     first_login = models.BooleanField(default=True)
     region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
