0c624a59314d6c97f6899486b817d71dbdd4b1ee
nakyeong <yunna3425@naver.com>
2024-02-19 20:12:56 +0900

    [feat] setting

--- server/config/settings.py
+++ server/config/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-09^97!th1@2z^k6v-_$bujzzk(oh53722q2_9#6x%o%0rp#thl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -147,26 +147,26 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # 1. local db.sqlite3 사용할 경우
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
     
 # 2. db 서버와 연결
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.environ.get('MYSQL_DBNAME'),
-#         'USER': os.environ.get('MYSQL_USERNAME'),
-#         'PASSWORD': os.environ.get('MYSQL_PASSWD'),
-#         'HOST': os.environ.get('MYSQL_HOST'),
-#         'PORT': int(os.environ.get('MYSQL_PORT')),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DBNAME'),
+        'USER': os.environ.get('MYSQL_USERNAME'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWD'),
+        'HOST': os.environ.get('MYSQL_HOST'),
+        'PORT': int(os.environ.get('MYSQL_PORT')),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -204,6 +204,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
